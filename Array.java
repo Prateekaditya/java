f2ef78592d010f1875c00cea8407bb6ab2cd78f3
Prateek Aditya <prateek0aditya@gmail.com>
2024-05-29 15:41:06 +0530

    intial commit

--- Array.java
+++ Array.java
@@ -8,6 +8,20 @@ public class Array{
         }
         return -1;
     }
+    public static int getLargest(int marks[]){
+        int lar=Integer.MIN_VALUE;
+        int small=Integer.MAX_VALUE;
+        for(int i=0;i<marks.length;i++){
+            if(lar<marks[i]){
+                lar=marks[i];
+            }
+            if(small>marks[i]){
+                small=marks[i];
+            }
+        }
+        System.out.println("smallest: "+small);
+        return lar;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in) ;
             System.out.println("The size of the array is :");
@@ -21,14 +35,16 @@ public class Array{
                 // marks[i]=sc.nextInt();
                 System.out.print(marks[i]+ " ");}
                 System.out.println(" ");
-                System.out.println("Enter the key you want to search: ");
-                int key=sc.nextInt();
-                int ls =linearsearch(marks,key);
-                if(ls==-1){
-                    System.out.println("Key Not found.");
-                }
-                else{
-                    System.out.println("Key found at index : " +ls);
-                }
+                // System.out.println("Enter the key you want to search: ");
+                // int key=sc.nextInt();
+                // int ls =linearsearch(marks,key);
+                // if(ls==-1){
+                //     System.out.println("Key Not found.");
+                // }
+                // else{
+                //     System.out.println("Key found at index : " +ls);
+                // }
+               int larmark=getLargest(marks);
+               System.out.println("Largest : "+larmark);
         }
     }
