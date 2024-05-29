925f90aa740d74a0b0175c04e47b2b508deefdc9
Prateek Aditya <prateek0aditya@gmail.com>
2024-05-29 14:59:56 +0530

    New array code updated

--- Array.java
+++ Array.java
@@ -1,5 +1,13 @@
 import java.util.*;
 public class Array{
+    public static int linearsearch(int marks[],int key){
+        for(int i=0;i<marks.length;i++){
+            if(marks[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in) ;
             System.out.println("The size of the array is :");
@@ -12,5 +20,15 @@ public class Array{
                for(int i=0;i<marks.length;i++){
                 // marks[i]=sc.nextInt();
                 System.out.print(marks[i]+ " ");}
+                System.out.println(" ");
+                System.out.println("Enter the key you want to search: ");
+                int key=sc.nextInt();
+                int ls =linearsearch(marks,key);
+                if(ls==-1){
+                    System.out.println("Key Not found.");
+                }
+                else{
+                    System.out.println("Key found at index : " +ls);
+                }
         }
     }
