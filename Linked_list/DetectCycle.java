b4a193e03422cb47871bdf16aa246acb04b6bf61
Prateek Aditya <137946231+Prateekaditya@users.noreply.github.com>
2025-01-10 01:05:58 +0530

    new commit

---
+++ Linked_list/DetectCycle.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class DetectCycle {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLL() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+
+        }
+        System.out.println("null");
+        return;
+    }
+
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        // head.next.next.next = head;
+        System.out.println(isCycle());
+    }
+}
